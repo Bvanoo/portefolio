b849c6da56d17b9cd20589545464febce8d356e3
bvanoo <bvanoostveldt@gmail.com[200~  git config --global user.email bvanoostveldt@gmail.com  git config --global user.email bvanoostveldt@gmail.com[200~>
2025-02-20 23:16:07 +0100

    Dropdown + shadow

--- js/script.js
+++ js/script.js
@@ -1,18 +1,27 @@
 let pos = 0;
+const arrow = document.getElementById("arrow");
+const header = document.querySelector("header");
+const profilName = document.querySelector(".profilName");
+const sub = document.querySelector(".sub");
+const profil = document.getElementById("photoProfil");
+const sndLi = document.querySelector(".sndLi");
 function backdrop(){
-    const arrow = document.getElementById("arrow");
-    const header = document.querySelector("header");
-    arrow.style.transformOrigin="center";
-    arrow.addEventListener('click',function(){
             if (pos===0){
-            header.style.transform = "translateX(-210px)";
-            arrow.style.transform = "translateX(80px)";
+            header.style.transform = "translateX(-190px)";
+            arrow.style.transform = "rotate(180deg)";
+            profil.style.transform ="translateX(180px) scale(.7)";
+            sub.style.opacity ="0";
+            profilName.style.opacity ="0";
+            sndLi.style.opacity ="0";
             pos=1;
-            console.log(pos);
-            arrow.style.transform="rotate(90deg)";
         } else {
             header.style.transform = "translateX(0px)";
-            arrow.style.transform="rotate(0deg)";
+            arrow.style.transform = "rotate(0deg)";
+            profil.style.transform ="translateX(0px)";
+            sub.style.opacity ="1";
+            profilName.style.opacity ="1";
+            sndLi.style.opacity ="1";
+
             pos=0;
         };
-})}
+}
